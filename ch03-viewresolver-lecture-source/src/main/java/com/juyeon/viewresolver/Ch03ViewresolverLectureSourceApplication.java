dd296c6b12daa08ac7d298bafef553e5bf3ae942
juyeon99 <beive999@naver.com>
2024-07-10 11:00:28 +0900

    view resolver

---
+++ ch03-viewresolver-lecture-source/src/main/java/com/juyeon/viewresolver/Ch03ViewresolverLectureSourceApplication.java
@@ -0,0 +1,13 @@
+package com.juyeon.viewresolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch03ViewresolverLectureSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch03ViewresolverLectureSourceApplication.class, args);
+    }
+
+}
