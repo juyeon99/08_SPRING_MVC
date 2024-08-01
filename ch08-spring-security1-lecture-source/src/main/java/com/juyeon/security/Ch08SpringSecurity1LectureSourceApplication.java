7327659ded8b103a8a61af5cbf435a019b69f2eb
juyeon99 <beive999@naver.com>
2024-08-01 18:10:03 +0900

    authentication

---
+++ ch08-spring-security1-lecture-source/src/main/java/com/juyeon/security/Ch08SpringSecurity1LectureSourceApplication.java
@@ -0,0 +1,13 @@
+package com.juyeon.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch08SpringSecurity1LectureSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch08SpringSecurity1LectureSourceApplication.class, args);
+    }
+
+}
